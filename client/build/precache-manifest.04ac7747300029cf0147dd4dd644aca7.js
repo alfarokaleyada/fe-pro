6e27e5d1dd142dda1e0b9789bb3cbe09c82821a5
alfarokaleyada <alfarok.aleyada@gmail.com>
2020-04-28 09:53:18 -0700

    make it better

---
+++ client/build/precache-manifest.04ac7747300029cf0147dd4dd644aca7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56245425ed69a528f13020f9bded96ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86b6e630e30cfc247ea6",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "79e88b67ec838e7bf76b",
+    "url": "/static/css/main.ed45d857.chunk.css"
+  },
+  {
+    "revision": "86b6e630e30cfc247ea6",
+    "url": "/static/js/2.6632b999.chunk.js"
+  },
+  {
+    "revision": "b1c48fb50b52ff7e51ea57a4c76b798d",
+    "url": "/static/js/2.6632b999.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79e88b67ec838e7bf76b",
+    "url": "/static/js/main.d7ce37b7.chunk.js"
+  },
+  {
+    "revision": "0f7e17235ce2106ccee1",
+    "url": "/static/js/runtime-main.549be430.js"
+  }
+]);
