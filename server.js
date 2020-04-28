7f176525fb478e8d172afc8f3212e8abe22f1982
alfarokaleyada <alfarok.aleyada@gmail.com>
2020-04-27 21:03:05 -0700

    make it better

--- server.js
+++ server.js
@@ -58,12 +58,12 @@ mongoose.connection.on('error', (err) => {
 
 // ----------- DB Middlewares --------- //
 app.use(cors())
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: false}));
 
 
 require('./config/passport')(passport);
